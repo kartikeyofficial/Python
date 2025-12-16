08dd6a40752d81c9fa89153f32b0556adc6cb6b8
kartikeyofficial <kartikeygithubproject@gmail.com>
2025-12-16 16:03:31 +0530

    Python

--- UNIT-3/Functions/**kwargs in Python (Keyword Arguments).py
+++
@@ -1,4 +0,0 @@
-def myprg(**kwargs):
-    for key, value in kwargs.items():
-        print(key,"==",value)
-myprg(first="Hello",mid="Welcome",last="Kartikey")
--- UNIT-3/Functions/*args in Python (Non- Keyword Arguments).py
+++
@@ -1,8 +0,0 @@
-def sum(*args):
-    resultfinal=0
-    for num in args:
-        resultfinal= resultfinal+num
-    return resultfinal
-print(sum(10,20,30))
-print(sum(10,20,30,40))
-print(sum(10,20,30,40,50))
